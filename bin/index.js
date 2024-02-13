e921d870930e887d16ec9e40567758903f518498
gotomi <gotomi@gotomi.info>
2024-02-13 23:13:08 +0100

    history API, WIP

--- bin/index.js
+++ bin/index.js
@@ -1,38 +1,46 @@
 #!/usr/bin/env node
-import { getReports, runQuery } from '../src/crux.js';
+import { getReports } from '../src/crux.js';
 import { printTable, printDistribution, printCSV, printHeading } from '../src/crux-output.js';
 import process from 'node:process';
-import path from 'path';
-import prependHttp from 'prepend-http';
 import minimist from 'minimist';
 
 const argv = minimist(process.argv.slice(2));
 
-function getDataFromUrls(file) {
-  try {
-    const urls = require(path.join(process.cwd(), file));
+function prepareQueryParams(argv) {
+  const effectiveConnectionType = !argv.history
+    ? {
+        effectiveConnectionType: argv.effectiveConnectionType || argv.ect || '',
+      }
+    : {};
 
-    if (!Array.isArray(urls)) throw new Error('Not an array');
-
-    return urls;
-  } catch (e) {
-    // process.exitCode = e.code;
-    throw new Error(e.code);
-  }
+  return {
+    ...effectiveConnectionType,
+    formFactor: argv.formFactor || 'PHONE',
+  };
 }
 
-const queryParams = {
-  formFactor: argv.formFactor || 'PHONE',
-  checkOrigin: argv.checkOrigin || false,
+const queryParams = prepareQueryParams(argv);
+const config = {
+  origin: argv.checkOrigin,
+  history: argv.history,
 };
 // input data validation
 const API_KEY = argv.key;
 const formFactorValues = ['ALL_FORM_FACTORS', 'PHONE', 'DESKTOP', 'TABLET'];
+const ectValues = ['offline', 'slow-2G', '2G', '3G', '4G'];
+
+if (queryParams.effectiveConnectionType && !ectValues.includes(queryParams.effectiveConnectionType)) {
+  console.log(
+    `parameter effectiveConnectionType should be one of allowed values:  ${ectValues.join(',')} or it can be skipped`,
+  );
+  process.exit();
+}
 
 if (!formFactorValues.includes(queryParams.formFactor)) {
   console.log(`parameter formFactor should be one of allowed values:  ${formFactorValues.join(',')}`);
   process.exit();
 }
+// validation end
 
 if (argv.help) {
   console.log(`
@@ -41,6 +49,7 @@ if (argv.help) {
   )} CrUX API key (required) (https://developers.google.com/web/tools/chrome-user-experience-report/api/guides/getting-started#APIKey)
   ${'--urls'.padEnd(25)} one or more comma seperated urls  (required)
   ${'--formFactor'.padEnd(25)} allowed values: ALL_FORM_FACTORS, DESKTOP, TABLET, PHONE (default)
+  ${'--ect'.padEnd(25)} allowed values: offline, slow-2G, 2G, 3G, 4G
   ${'--checkOrigin'.padEnd(25)} get CrUX data for origin instead of given url
   ${'--output'.padEnd(25)} posible values: distribution, json, csv, table (default) 
 
@@ -56,51 +65,39 @@ Usage:
 }
 
 if (!argv.key) {
-  console.log(`Provide an API key to make CrUX Rest API request \n '--key=[YOUR_API_KEY]'}`);
+  console.log(`Provide an API key to make CrUX Rest API request  '--key=[YOUR_API_KEY]'}`);
   process.exit();
 }
 
-if (!argv.urls && !argv.file) {
+if (!argv.urls) {
   console.error(
     `Provide either urls or file argument \n${'--urls www.google.com,www.bing.com'}\n or JSON file \n${'--file path_to_json_file'}`,
   );
   process.exit();
 }
 
-let urls;
-
-if (argv.urls) {
-  urls = argv.urls.split(',').map((url) => prependHttp(url));
-} else {
-  urls = getDataFromUrls(argv.file).map((url) => prependHttp(url));
-}
-
+const urls = argv.urls;
 //output
 
-argv.history &&
-  runQuery(urls, API_KEY, queryParams, true).then((cruxData) => {
-    console.log(JSON.stringify(cruxData));
-  });
+getReports(urls, API_KEY, queryParams, config).then((cruxData) => {
+  if (cruxData.length === 0) {
+    console.log(`There's no valid data`);
+    process.exitCode = 0;
+  }
 
-!argv.history &&
-  getReports(urls, API_KEY, queryParams).then((cruxData) => {
-    if (cruxData.length === 0) {
-      console.log(`There's no valid data`);
-      process.exitCode = 0;
-    }
+  const data = cruxData;
 
-    const data = cruxData;
-
-    if (argv.output === 'json') {
-      console.log(JSON.stringify(data));
-    } else if (argv.output === 'csv') {
-      printHeading(data.params);
-      printCSV(data.metrics);
-    } else if (argv.output === 'distribution') {
-      printHeading(data.params);
-      printDistribution(data.metrics);
-    } else {
-      printHeading(data.params);
-      printTable(data.metrics);
-    }
-  });
+  if (argv.output === 'json' || argv.history) {
+    //todo - other view for history data
+    console.log(JSON.stringify(data));
+  } else if (argv.output === 'csv') {
+    printHeading(data.params);
+    printCSV(data.metrics);
+  } else if (argv.output === 'distribution') {
+    printHeading(data.params);
+    printDistribution(data.metrics);
+  } else {
+    printHeading(data.params);
+    printTable(data.metrics);
+  }
+});
--- src/crux.js
+++ src/crux.js
@@ -1,37 +1,33 @@
 import { google } from 'googleapis';
 import { convertData } from './crux-convert.js';
+import prependHttp from 'prepend-http';
 
-export async function runQuery(url, API_KEY, queryParams, historyAPI = false) {
-  const params = JSON.parse(JSON.stringify(queryParams));
-
-  if (params.checkOrigin) {
-    params.origin = url;
-  } else {
-    params.url = url;
-  }
-  delete params.checkOrigin; // cleanup
-
+export async function runQuery(url, API_KEY, queryParams, config) {
+  const urlOrOrigin = config.origin ? { origin: url } : { url: url };
+  const params = { ...queryParams, ...urlOrOrigin };
   const crux = google.chromeuxreport({
     version: 'v1',
     auth: API_KEY,
   });
-  const res = historyAPI ? await crux.records.queryHistoryRecord(params) : await crux.records.queryRecord(params);
+  const res = config.history ? await crux.records.queryHistoryRecord(params) : await crux.records.queryRecord(params);
 
   return res.data.record;
 }
 
-function generateTasks(urls, API_KEY, queryParams) {
+function generateTasks(urls, API_KEY, queryParams, config) {
+  const urlsPrepared = urls.split(',').map((url) => prependHttp(url));
+
   let tasks = [];
-  urls.forEach((url) =>
-    tasks.push(runQuery(url, API_KEY, queryParams).catch((error) => console.error('❌ ', url, error.errors))),
+  urlsPrepared.forEach((url) =>
+    tasks.push(runQuery(url, API_KEY, queryParams, config).catch((error) => console.error('❌ ', url, error.errors))),
   );
 
   return tasks;
 }
 
-export async function getReports(urls, API_KEY, queryParams) {
-  const tasks = generateTasks(urls, API_KEY, queryParams);
+export async function getReports(urls, API_KEY, queryParams, config) {
+  const tasks = generateTasks(urls, API_KEY, queryParams, config);
   const data = (await Promise.all([...tasks])).filter((item) => !!item);
 
-  return data.length > 0 ? convertData(data) : [];
+  return data.length && !config.history ? convertData(data) : data;
 }
