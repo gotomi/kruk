5310598573f02b96fca39f44e5802ea36f00e5ab
gotomi <gotomi@gotomi.info>
2025-02-08 22:48:22 +0100

    eslint config update

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  eslintPluginPrettierRecommended,
+];
